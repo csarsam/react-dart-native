8848e59da6da4915bba3b02cba8af5ead880b29f
Claire Sarsam <csarsam@asu.edu>
2015-04-15 21:04:24 -0700

    Use the new StyleSheet api for styling in SampleApp

--- example/SampleApp/dart/index.dart
+++ example/SampleApp/dart/index.dart
@@ -1,36 +1,40 @@
 import 'package:react/react.dart';
 import 'package:react/react_client.dart';
 
+import 'dart:js';
+
 class SampleApp extends Component {
   render() {
-    return View({'style': containerStyle}, [
-      Text({'style': welcomeStyle}, 'Welcome to Dart React Native!'),
-      Text({'style': instructionsStyle}, 'To get started, edit index.dart'),
-      Text({'style': instructionsStyle}, 'Press Cmd+R to reload,\nCmd+Control+Z for dev menu')
+    return View({'style': styleSheet['containerStyle']}, [
+      Text({'style': styleSheet['welcomeStyle']}, 'Welcome to Dart React Native!'),
+      Text({'style': styleSheet['instructionsStyle']}, 'To get started, edit index.dart'),
+      Text({'style': styleSheet['instructionsStyle']}, 'Press Cmd+R to reload,\nCmd+Control+Z for dev menu')
     ]);
   }
 }
 
 main() {
   setClientConfiguration();
+  styleSheet = StyleSheet.create({
+      'welcomeStyle': {
+          'fontSize': 20,
+          'textAlign': 'center',
+          'margin': 10
+      },
+      'containerStyle': {
+        'flex': 1,
+        'justifyContent': 'center',
+        'alignItems': 'center',
+        'backgroundColor': '#F5FCFF'
+      },
+      'instructionsStyle': {
+        'textAlign': 'center',
+        'color': '#333333',
+        'marginBottom': 5
+      }
+  });
+
   registerComponent('SampleApp', () => new SampleApp(), true);
 }
 
-Map containerStyle = {
-  'flex': 1,
-  'justifyContent': 'center',
-  'alignItems': 'center',
-  'backgroundColor': '#F5FCFF'
-};
-
-Map welcomeStyle = {
-  'fontSize': 20,
-  'textAlign': 'center',
-  'margin': 10
-};
-
-Map instructionsStyle = {
-  'textAlign': 'center',
-  'color': '#333333',
-  'marginBottom': 5
-};
+JsObject styleSheet;
